162b2c7806bbe497cd34052d11178c690c86da8d
wojkly <wojkly@gmail.com>
2022-03-25 23:40:47 +0100

    #44 add some dummy classes

---
+++ emt-server/src/main/java/com/agh/emt/service/MyService.java
@@ -0,0 +1,12 @@
+package com.agh.emt.service;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class MyService {
+    public String myServiceMethod() {
+        return "EMT EMT EMT";
+    }
+}
