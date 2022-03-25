9d74eb5ab600001c111e7811740d5d9fc50c1269
wojkly <wojkly@gmail.com>
2022-03-25 23:35:11 +0100

    #44 create spring project template, move gitignore to root

---
+++ emt-server/src/main/java/com/agh/emt/EmtApplication.java
@@ -0,0 +1,13 @@
+package com.agh.emt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmtApplication.class, args);
+	}
+
+}
