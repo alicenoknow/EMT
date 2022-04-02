75d72bf68221eab2182f15c3d45a2df52bba6c01
Alicja Niewiadomska <micek2000@gmail.com>
2022-04-02 13:02:53 +0200

    Refactor pages layout

--- emt-client/src/components/NavBar.tsx
+++ emt-client/src/components/NavBar.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 export default function NavBar() {
 	return (
-		<Navbar bg="light" variant="light">
-			<Container>
+		<Navbar>
 				<Navbar.Brand href="/">
 					<img
 						alt=""
@@ -20,7 +19,6 @@ export default function NavBar() {
 					<Nav.Link href="/news">Aktualności</Nav.Link>
 					<Nav.Link href="/login/user">Rekrutacja</Nav.Link>
 				</Nav>
-			</Container>
 		</Navbar>
 	);
 }
--- emt-client/src/index.tsx
+++ emt-client/src/index.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import NavBar from "./components/NavBar";
 import Login from "./pages/Login";
+import ScreenWrapper from './components/ScreenWrapper';
 import RecruitSurveyForm from "./pages/RecruitSurveyForm";
 import UserPage from "./pages/UserPage";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<NavBar />
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/user" element={<UserPage firstName="Anna" lastName="Kowalska"/>} />
-				<Route path="/form" element={<RecruitSurveyForm />} />
-			</Routes>
-		</BrowserRouter>
+		<ScreenWrapper>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/user" element={<UserPage firstName="Anna" lastName="Kowalska"/>} />
+					<Route path="/form" element={<RecruitSurveyForm />} />
+				</Routes>
+			</BrowserRouter>
+		</ScreenWrapper>
 	</React.StrictMode>,
 	document.getElementById("root"),
 );
