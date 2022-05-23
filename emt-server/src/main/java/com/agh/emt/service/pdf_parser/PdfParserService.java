432a1f481f186f2cd0cbb39b20c82cb2b3a442e6
kwrzesni <kamil.wrzesniak@onet.pl>
2022-05-23 11:29:23 +0200

    pdfParserService methods work on separated files

--- emt-server/src/main/java/com/agh/emt/service/pdf_parser/PdfParserService.java
+++ emt-server/src/main/java/com/agh/emt/service/pdf_parser/PdfParserService.java
@@ -21,19 +21,19 @@ public class PdfParserService {
 
     public PdfData getDataFromPdf(byte[] pdf) throws IOException, InterruptedException {
         // create temp dirs
-        File tempPdf = new File(".tempPdf");
-        File tempJson = new File(".tempJson");
+        File tempPdf = new File(".getDataFromPdf_tempPdf");
+        File tempJson = new File(".getDataFromPdf_tempJson");
         tempPdf.mkdir();
         tempJson.mkdir();
 
         // write pdf to temp file
-        FileUtils.writeByteArrayToFile(new File(".tempPdf/temp"), pdf);
+        FileUtils.writeByteArrayToFile(new File(".getDataFromPdf_tempPdf/temp"), pdf);
 
         // convert pdf to json
         String[] command = {"python",
                             pythonScriptsRoot + "pdf_to_json.py",
-                            ".tempPdf/",
-                            ".tempJson/"};
+                            ".getDataFromPdf_tempPdf/",
+                            ".getDataFromPdf_tempJson/"};
         Process process = Runtime.getRuntime().exec(command);
         process.waitFor();
         System.out.println(new String(process.getErrorStream().readAllBytes()));
@@ -41,7 +41,7 @@ public class PdfParserService {
         System.out.println(new String(process.getInputStream().readAllBytes()));
 
         // read json
-        String jsonString = new String(Files.readAllBytes(Paths.get(".tempJson/temp_form.json")), StandardCharsets.UTF_8);
+        String jsonString = new String(Files.readAllBytes(Paths.get(".getDataFromPdf_tempJson/temp_form.json")), StandardCharsets.UTF_8);
         JSONObject jsonObject = new JSONObject(jsonString);
         PdfData pdfData = new PdfData(jsonObject.getString("Imie"),
                                       jsonObject.getString("Nazwisko"),
@@ -51,8 +51,8 @@ public class PdfParserService {
         // delete temp folder
         FileUtils.deleteDirectory(tempPdf);
         FileUtils.deleteDirectory(tempJson);
-        (new File(".tempJson/temp_form.json")).delete();
-        (new File(".tempPdf/temp")).delete();
+        (new File(".getDataFromPdf_tempJson/temp_form.json")).delete();
+        (new File(".getDataFromPdf_tempPdf/temp")).delete();
 
         // return result
         return pdfData;
@@ -60,15 +60,15 @@ public class PdfParserService {
 
     public byte[] parserPdfsToExcel(List<byte[]> pdfs) throws IOException, InterruptedException {
         // create temp dirs
-        File tempPdf = new File(".tempPdf");
-        File tempJson = new File(".tempJson");
+        File tempPdf = new File(".parserPdfsToExcel_tempPdf");
+        File tempJson = new File(".parserPdfsToExcel_tempJson");
         tempPdf.mkdir();
         tempJson.mkdir();
 
         // write pdfs to folder
         int i = 0;
         for (byte[] pdf : pdfs) {
-            FileUtils.writeByteArrayToFile(new File(".tempPdf/" + Integer.toString(i)), pdf);
+            FileUtils.writeByteArrayToFile(new File(".parserPdfsToExcel_tempPdf/" + Integer.toString(i)), pdf);
             ++i;
         }
 
@@ -77,9 +77,9 @@ public class PdfParserService {
         // convert pdf to json
         String[] command = {"python",
                             pythonScriptsRoot + "pdf_form_extractor.py",
-                            ".temp.csv",
-                            ".tempPdf/",
-                            ".tempJson/"};
+                            ".parserPdfsToExcel_temp.csv",
+                            ".parserPdfsToExcel_tempPdf/",
+                            ".parserPdfsToExcel_tempJson/"};
         Process process = Runtime.getRuntime().exec(command);
         process.waitFor();
 
@@ -88,12 +88,12 @@ public class PdfParserService {
         System.out.println(new String(process.getInputStream().readAllBytes()));
 
         // read csv
-        byte[] csvData = Files.readAllBytes(Path.of(".temp.csv"));
+        byte[] csvData = Files.readAllBytes(Path.of(".parserPdfsToExcel_temp.csv"));
 
         // delete temp files
         FileUtils.deleteDirectory(tempPdf);
         FileUtils.deleteDirectory(tempJson);
-        (new File(".temp.csv")).delete();
+        (new File(".parserPdfsToExcel_temp.csv")).delete();
 
         // return result
         return csvData;
