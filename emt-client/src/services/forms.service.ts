b04045551c0dc08f6e8f69a8bbe03fb5340464e8
Byczong <piotrzwsln8@gmail.com>
2022-05-30 01:32:23 +0200

    FE download sent form

--- emt-client/src/services/forms.service.ts
+++ emt-client/src/services/forms.service.ts
@@ -7,6 +7,8 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 const ROOT_API = "http://localhost:8080/api/recruitment-form/";
 const TEMPLATE_FORM_API = "default";
 const SEND_FORM_API = "my-form";
+const GET_PDF_API = "my-form/download/";
+const GET_SCAN_API = "my-form/scan/download/";
 
 export const getTemplate = (): Promise<void> => {
 	const tokenStr = JSON.parse(String(localStorage.getItem("user")))?.token;
@@ -51,4 +53,60 @@ export const sendFilledPdf = (pdf: FormData): Promise<number> => {
 			}
 			return undefined;
 		});
+};
+
+export const getSentPdf = (priority: number): Promise<void> => {
+	const tokenStr = JSON.parse(String(localStorage.getItem("user")))?.token;
+
+	return axios
+		.get(ROOT_API + GET_PDF_API + priority.toString(), {
+			headers: {
+				Authorization: `Bearer ${tokenStr}`,
+			},
+			responseType: "blob",
+		})
+		.catch(function (error) {
+			console.log(error.toJSON());
+			return undefined;
+		})
+		.then(response => {
+			if (response?.data) {
+				FileDownload(response.data, "AnkietaRekrutacyjnaErasmus2022-pr-" + priority.toString() + ".pdf");
+			}
+		});
+};
+
+export const getSentScan = (priority: number): Promise<void> => {
+	const tokenStr = JSON.parse(String(localStorage.getItem("user")))?.token;
+
+	return axios
+		.get(ROOT_API + GET_SCAN_API + priority.toString(), {
+			headers: {
+				Authorization: `Bearer ${tokenStr}`,
+			},
+			responseType: "blob",
+		})
+		.catch(function (error) {
+			console.log(error.toJSON());
+			return undefined;
+		})
+		.then(response => {
+			if (response?.data) {
+				FileDownload(response.data, "AnkietaRekrutacyjnaErasmus2022-skan-pr" + priority.toString() + ".pdf");
+			}
+		});
+};
+
+export const getUserForm = (priority: number) => {
+	const tokenStr = JSON.parse(String(localStorage.getItem("user")))?.token;
+
+	return axios
+		.get(ROOT_API + SEND_FORM_API + '/' + priority.toString(), {
+			headers: { Authorization: `Bearer ${tokenStr}` },
+		})
+		.catch(function (error) {
+			console.log(error.toJSON());
+			return undefined;
+		})
+		.then(response => response?.data);
 };
