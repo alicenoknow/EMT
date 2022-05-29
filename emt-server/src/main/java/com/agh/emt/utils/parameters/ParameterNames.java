68442fd8a9b06d4a30635281e5e0f0ee000935ea
Wojciech Kłyszejko <wojkly@gmail.com>
2022-05-29 12:36:28 +0200

    add parameter handling on BE

---
+++ emt-server/src/main/java/com/agh/emt/utils/parameters/ParameterNames.java
@@ -0,0 +1,6 @@
+package com.agh.emt.utils.parameters;
+
+public class ParameterNames {
+    public static final String RECRUITMENT_START_DATE = "DATA_POCZATEK_REKRUTACJI";
+    public static final String RECRUITMENT_END_DATE = "DATA_KONIEC_REKRUTACJI";
+}
