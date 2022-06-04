3ed6de28e433ae19e8a28dc9efff6e4ecf1fa956
Wojciech Klyszejko <wojkly@gmail.com>
2022-06-04 18:33:01 +0200

    add date validation, add new parameter - max number of recruitment forms per student

--- emt-server/src/main/java/com/agh/emt/utils/parameters/ParameterNames.java
+++ emt-server/src/main/java/com/agh/emt/utils/parameters/ParameterNames.java
@@ -1,6 +1,7 @@
 package com.agh.emt.utils.parameters;
 
 public class ParameterNames {
-    public static final String RECRUITMENT_START_DATE = "DATA_POCZATEK_REKRUTACJI";
-    public static final String RECRUITMENT_END_DATE = "DATA_KONIEC_REKRUTACJI";
+    public static final String RECRUITMENT_START_DATE = "RECRUITMENT_START_DATE";
+    public static final String RECRUITMENT_END_DATE = "RECRUITMENT_END_DATE";
+    public static final String MAX_RECUITMENT_FORMS_PER_STUDENT = "MAX_RECUITMENT_FORMS_PER_STUDENT";
 }
