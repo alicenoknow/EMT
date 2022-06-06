709b79c7de5482c8dadd7036d94658475526f6dd
Alicja Niewiadomska <micek2000@gmail.com>
2022-06-06 10:01:41 +0200

    Forms and date setup

--- emt-client/src/components/admin/AdminSettings.tsx
+++ emt-client/src/components/admin/AdminSettings.tsx
@@ -5,12 +5,16 @@ import "./AdminSettings.scss";
 import "react-datepicker/dist/react-datepicker.css";
 import { getTemplate } from "../../services/forms.service";
 import { sendDefaultPdf } from "../../services/admin.service";
+import { TextField } from "@material-ui/core";
+import { setParam } from "../../services/params.service";
+import Loading from "../Loading";
 
 interface AdminSettingsProps {
 	config?: {
 		startDate?: Date;
 		endDate?: Date;
 		defaultPDFLink?: string;
+		formsNum?: number;
 	};
 }
 
@@ -23,6 +27,14 @@ export default function AdminSettings(props: AdminSettingsProps) {
 	const [endDate, setEndDate] = useState<MaybeDate>(props?.config?.endDate);
 	const [uploadedFileName, setUploadedFileName] = useState<string | null>(null);
 	const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+	const [formsNum, setFormsNum] = useState<number>(
+		props?.config?.formsNum ?? 2,
+	);
+	const [confirm, setConfirmLoading] = useState<{
+		date: boolean;
+		forms: boolean;
+		doc: boolean;
+	}>({ date: false, forms: false, doc: false });
 
 	const inputRef = useRef<HTMLInputElement>(null);
 
@@ -40,16 +52,63 @@ export default function AdminSettings(props: AdminSettingsProps) {
 	const handleFileSend = async () => {
 		const formData = new FormData();
 		formData.append("pdf", uploadedFile ?? "");
+		setConfirmLoading({ ...confirm, doc: false });
 		await sendDefaultPdf(formData);
+		setTimeout(() => setConfirmLoading({ ...confirm, doc: true }), 500);
 	};
 
-	const handleDateChange = () => {
-		// TODO handle date update
-		return 0;
+	const convertDateToString = (date: Date): string => {
+		return `${date.getFullYear()}-${String(date.getMonth()).padStart(
+			2,
+			"0",
+		)}-${String(date.getDay()).padStart(2, "0")}`;
+	};
+
+	const handleDateChange = async () => {
+		if (startDate && endDate) {
+			setConfirmLoading({ ...confirm, date: false });
+			await setParam("RECRUITMENT_START_DATE", convertDateToString(startDate));
+			await setParam("RECRUITMENT_END_DATE", convertDateToString(endDate));
+			setTimeout(() => setConfirmLoading({ ...confirm, date: true }), 500);
+		}
+	};
+
+	const handleFormsNumChange = async () => {
+		if (formsNum) {
+			setConfirmLoading({ ...confirm, forms: false });
+			await setParam("MAX_RECUITMENT_FORMS_PER_STUDENT", formsNum?.toString());
+			setTimeout(() => setConfirmLoading({ ...confirm, forms: true }), 500);
+		}
 	};
 
 	return (
 		<div className="settings">
+			<div className="forms_input">
+				<h4>Zmień dostepną liczbę ankiet dla studenta:</h4>
+				<TextField
+					id="outlined-number"
+					label="Liczba ankiet"
+					type="number"
+					value={formsNum}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						const val = parseInt(e.currentTarget.value);
+						if (val >= 0) {
+							setFormsNum(val);
+						}
+					}}
+					InputLabelProps={{
+						shrink: true,
+					}}
+				/>
+				<Button
+					onClick={handleFormsNumChange}
+					className={"send__button"}
+					disabled={!formsNum}>
+					Zatwierdź
+				</Button>
+				{confirm.forms && <>✅</>}
+			</div>
+			<div className="document__line" />
 			<div className="data__container">
 				<h4>Zmień datę rozpoczęcia rekrutacji:</h4>
 				<DatePicker onChange={setStartDate} selected={startDate} />
@@ -58,12 +117,10 @@ export default function AdminSettings(props: AdminSettingsProps) {
 				<h4>Zmień datę zakończenia rekrutacji:</h4>
 				<DatePicker onChange={setEndDate} selected={endDate} />
 			</div>
-			<Button
-				onClick={handleDateChange}
-				className={"send__button"}
-				disabled={!!endDate && !!startDate}>
+			<Button onClick={handleDateChange} className={"send__button"}>
 				Zatwierdź
 			</Button>
+			{confirm.date && <>✅</>}
 			<div className="document__line" />
 			<h4>Pobierz dokument:</h4>
 			<Button onClick={getTemplate} className={"upload__button"}>
@@ -87,6 +144,7 @@ export default function AdminSettings(props: AdminSettingsProps) {
 					disabled={!uploadedFileName}>
 					Prześlij dokument
 				</Button>
+				{confirm.doc && <>✅</>}
 			</div>
 		</div>
 	);
--- emt-client/src/pages/AdminPage.tsx
+++ emt-client/src/pages/AdminPage.tsx
@@ -16,20 +16,42 @@ import { FormApiListRecord, StudentFormRecord } from "../types/admin";
 import Loading from "../components/Loading";
 import "./AdminPage.scss";
 import { getFormList, getResults } from "../services/admin.service";
+import { getParam } from "../services/params.service";
 
 export default function AdminPage() {
 	const [tabIndex, setTabIndex] = useState<number>(0);
 	const [resultsLink, setResultsLink] = useState<string | undefined>();
 	const [formList, setFormList] = useState<StudentFormRecord[] | undefined>();
+	const [startDate, setStartDate] = useState<Date | undefined>();
+	const [endDate, setEndDate] = useState<Date | undefined>();
+	const [formsNum, setFormsNum] = useState<number | undefined>();
 
 	useEffect(() => {
 		fetchResultsLink();
 		fetchFormList();
+		fetchStartDate();
+		fetchEndDate();
+		fetchFormsNum();
 	}, []);
 
 	const fetchResultsLink = async () => {
 		const result = await getResults();
 		setResultsLink(result.oneDriveLink);
+	};
+
+	const fetchStartDate = async () => {
+		const result = await getParam("RECRUITMENT_START_DATE");
+		setStartDate(new Date(result?.value));
+	};
+
+	const fetchEndDate = async () => {
+		const result = await getParam("RECRUITMENT_END_DATE");
+		setEndDate(new Date(result?.value));
+	};
+
+	const fetchFormsNum = async () => {
+		const result = await getParam("MAX_RECUITMENT_FORMS_PER_STUDENT");
+		setFormsNum(parseInt(result?.value));
 	};
 
 	const fetchFormList = async () => {
@@ -79,7 +101,7 @@ export default function AdminPage() {
 					<AdminResults resultsLink={resultsLink} />
 				</TabPanel>
 				<TabPanel value={tabIndex} index={2}>
-					<AdminSettings />
+					<AdminSettings config={{ startDate, endDate, formsNum }} />
 				</TabPanel>
 				<TabPanel value={tabIndex} index={3}>
 					<AdminHelp />
